78057c88702280a85c4aee607b67c219f9cd0023
Édnei Cristian <ednei.cristian@gmail.com>
2021-09-27 01:41:22 -0300

    Sites Product-detail_Parte1

--- vendor/hcodebr/php-classes/src/Model/Product.php
+++ vendor/hcodebr/php-classes/src/Model/Product.php
@@ -12,37 +12,30 @@ class Product extends Model {
 		
 		return $sql->select ( "SELECT * FROM tb_products ORDER BY desproduct" );
 	}
-	
-	public static function checklist($list){
-		
-		foreach ($list as &$row){
+	public static function checklist($list) {
+		foreach ( $list as &$row ) {
 			
-			$p =new Product();
-			$p ->setData($row);
-			$row = $p->getValues();			
+			$p = new Product ();
+			$p->setData ( $row );
+			$row = $p->getValues ();
 		}
 		
 		return $list;
-		
 	}
-	
-	
-	
-	
 	public function save() {
 		$sql = new Sql ();
 		
 		$results = $sql->select ( "CALL sp_products_save(:idproduct, :desproduct, :vlprice, :vlwidth, :vlheight, :vllength,
 				:vlweight, :desurl)", array (
 				
-				":idproduct" => $this->getidproduct(),
-				":desproduct" => $this->getdesproduct(),
-				":vlprice" => $this->getvlprice(),
-				":vlwidth" => $this->getvlwidth(),
-				":vlheight" => $this->getvlheight(),
-				":vllength" => $this->getvllength(),
-				":vlweight" => $this->getvlweight(),
-				":desurl" => $this->getdesurl() 
+				":idproduct" => $this->getidproduct (),
+				":desproduct" => $this->getdesproduct (),
+				":vlprice" => $this->getvlprice (),
+				":vlwidth" => $this->getvlwidth (),
+				":vlheight" => $this->getvlheight (),
+				":vllength" => $this->getvllength (),
+				":vlweight" => $this->getvlweight (),
+				":desurl" => $this->getdesurl () 
 		) );
 		
 		$this->setData ( $results [0] );
@@ -56,83 +49,83 @@ class Product extends Model {
 		
 		$this->setData ( $results [0] );
 	}
-	
 	public function delete() {
 		$sql = new Sql ();
 		
 		$sql->execquery ( "DELETE FROM tb_products WHERE idproduct = :idproduct", [ 
 				':idproduct' => $this->getidproduct () 
-		]);
-				
+		] );
 	}
-	
-	public function checkPhoto(){
-		
-		if (file_exists($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.
-				"res".DIRECTORY_SEPARATOR.
-				"site".DIRECTORY_SEPARATOR. 
-				"img".DIRECTORY_SEPARATOR. 
-				"products".DIRECTORY_SEPARATOR. 
-				$this->getidproduct().".jpg"				
-				)) {
-					$url="/res/site/img/products/". $this->getidproduct().".jpg";					
-				} else{
-					
-					$url="/res/site/img/product.jpg";
-				}
-				return $this->setdesphoto($url);		
+	public function checkPhoto() {
+		if (file_exists ( $_SERVER ['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "site" . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "products" . DIRECTORY_SEPARATOR . $this->getidproduct () . ".jpg" )) {
+			$url = "/res/site/img/products/" . $this->getidproduct () . ".jpg";
+		} else {
+			
+			$url = "/res/site/img/product.jpg";
+		}
+		return $this->setdesphoto ( $url );
 	}
-	
-	
-	public function getValues(){
+	public function getValues() {
+		$this->checkphoto ();
 		
-		$this->checkphoto();
-		
-		$values = parent::getValues();
+		$values = parent::getValues ();
 		
 		return $values;
 	}
-	
-	public function setPhoto($file){
+	public function setPhoto($file) {
+		$extension = explode ( '.', $file ['name'] );
 		
-		$extension= explode ('.', $file['name']);
+		$extension = end ( $extension );
 		
-		$extension = end($extension);
-		
-		switch ($extension){
+		switch ($extension) {
 			
-			case"jpg";
-			case"jpeg";
-			$image = imagecreatefromjpeg($file["tmp_name"]);
-			break;
+			case "jpg" :
+			case "jpeg" :
+				$image = imagecreatefromjpeg ( $file ["tmp_name"] );
+				break;
 			
-			case"gif";
-			$image = imagecreatefromgif($file["tmp_name"]);
+			case "gif" :
+				$image = imagecreatefromgif ( $file ["tmp_name"] );
+				
+				break;
 			
-			break;
-			
-			case"png";
-			$image = imagecreatefrompng($file["tmp_name"]);
-			
-			break;
-						
+			case "png" :
+				$image = imagecreatefrompng ( $file ["tmp_name"] );
+				
+				break;
 		}
 		
-		$dist= $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.
-				"res".DIRECTORY_SEPARATOR.
-				"site".DIRECTORY_SEPARATOR. 
-				"img".DIRECTORY_SEPARATOR. 
-				"products".DIRECTORY_SEPARATOR. 
-				$this->getidproduct().".jpg";
+		$dist = $_SERVER ['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "site" . DIRECTORY_SEPARATOR . "img" . DIRECTORY_SEPARATOR . "products" . DIRECTORY_SEPARATOR . $this->getidproduct () . ".jpg";
 		
-		imagejpeg($image, $dist);
+		imagejpeg ( $image, $dist );
 		
-		imagedestroy($image);
+		imagedestroy ( $image );
 		
-		$this->checkPhoto();
+		$this->checkPhoto ();
+	}
+	public function getFromURL($desurl) {
+		$sql = new Sql ();
 		
+		$rows = $sql->select ( "SELECT * FROM tb_products WHERE desurl = :desurl LIMIT 1", [ 
+				':desurl' => $desurl 
+		]
+		 );
+		
+		$this->setData ( $rows [0] );
 	}
 	
+	public function getGategories(){
+		
+	$sql=new Sql();
+	
+	return $sql->select("SELECT * FROM tb_categories a 
+						INNER JOIN tb_productscategories b 
+						ON a.idcategory = b.idcategoty
+						WHERE b.idproduct = :idproduct",[
+							':idproduct'=>$this->getidproduct()
+						]);
+		
+	}
 }
 
 ?>
