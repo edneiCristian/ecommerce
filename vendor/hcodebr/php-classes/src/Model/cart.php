42e1589a489fc6e5b2ec3ab0302b98247a9334a6
Ednei Cristian <ednei.cristian@gmail.com>
2021-09-29 07:04:57 -0300

    Atualizando

--- vendor/hcodebr/php-classes/src/Model/cart.php
+++ vendor/hcodebr/php-classes/src/Model/cart.php
@@ -4,107 +4,143 @@ namespace Hcode\Model;
 use Hcode\DB\Sql;
 use Hcode\Model;
 use Hcode\Mailer;
-use \Hcode\Model\User;
+use Hcode\Model\User;
 
-class Cart extends Model {
-    
+class Cart extends Model
+{
+
     const SESSION = "Cart";
-    
-    public static function getFromSession(){
-        
-        $cart =new Cart();
-        
-        if (isset($_SESSION[Cart::SESSION]) && (int)$_SESSION[Cart::SESSION]['idcart']>0){
-            
-            $cart->get((int)$_SESSION[Cart::SESSION]['idcart']);
-            
+
+    public static function getFromSession()
+    {
+        $cart = new Cart();
+
+        if (isset($_SESSION[Cart::SESSION]) && (int) $_SESSION[Cart::SESSION]['idcart'] > 0) {
+
+            $cart->get((int) $_SESSION[Cart::SESSION]['idcart']);
         } else {
-            
+
             $cart->getFromSessionID();
-            
-            if (!(int)$cart->getidcart()>0){
-                
-                $data=[
-                    'dessessionid'=>session_id()
+
+            if (! (int) $cart->getidcart() > 0) {
+
+                $data = [
+                    'dessessionid' => session_id()
                 ];
-                
+
                 if (User::checkLogin(false)) {
-                    
-                    $user=User::getFromSession();
-                    }
-                
+
+                    $user = User::getFromSession();
+                }
+
                 $cart->setData($data);
-                
+
                 $cart->save();
-                
+
                 $cart->setToSession();
             }
-            
         }
-            
+
         return $cart;
-   }
-   
-   public function setToSession(){
-       
-       $_SESSION[Cart::SESSION]=$this->getValues();
-       
-   }
-   
-    
-   
-   public function getFromSessionID() {
-
-    $sql = new Sql();
-
-    $results = $sql->select("SELECT * FROM tb_carts WHERE dessessionid= :dessessionid", [
-        ':dessessionid' => session_id()
-    ]);
-
-    if (count($results) > 0) {
-        $this->setData($results[0]);
     }
-    
-}
-    
-    public function get(int $idcart) {
-        
-        $sql= new Sql();
-        
-        $results = $sql->select("SELECT * FROM tb_carts WHERE idcart= :idcart",[
-            ':idcart'=>$idcart
-            
-            
+
+    public function setToSession()
+    {
+        $_SESSION[Cart::SESSION] = $this->getValues();
+    }
+
+    public function getFromSessionID()
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_carts WHERE dessessionid= :dessessionid", [
+            ':dessessionid' => session_id()
         ]);
-        
-        if (count($results)>0){
-        $this->setData($results[0]);
+
+        if (count($results) > 0) {
+            $this->setData($results[0]);
         }
-    
+    }
+
+    public function get(int $idcart)
+    {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_carts WHERE idcart= :idcart", [
+            ':idcart' => $idcart
+        ]);
+
+        if (count($results) > 0) {
+            $this->setData($results[0]);
+        }
+    }
+
+    public function save()
+    {
+        $sql = new Sql();
+
+        $result = $sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduse, :deszipcode, :vlfreight, :nrdays)", [
+
+            ':idcart' => $this->getidcart(),
+            ':dessessionid' => $this->getdessessionid(),
+            ':iduse' => $this->getiduse(),
+            ':deszipcode' => $this->getdeszipcode(),
+            ':vlfreight' => $this->getvlfreight(),
+            ':nrdays' => $this->getnrdays()
+        ]);
+
+        $this->setData($result[0]);
+    }
+
+    public function addProduct(Product $product)
+    {
+        $sql = new Sql();
+
+        $sql->execquery("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES(:idcard, :idproduct)", [
+
+            ':idcart' => $this->getidcart(),
+            ':idproduct' => $product->getidproduct()
+        ]);
+    }
+
+    public function removeProduct(Product $product, $all = false)
+    {
+        $sql = new Sql();
+
+        if ($all) {
+            $sql->execquery("UPDATE tb_cartsproducts SET dtremoved=NOW() WHERE idcart = :idcart AND idproduct =:idproduct AND
+                dtremoved IS NULL", [
+                ':idcart' => $this->getidcart(),
+                ':idproduct' => $PRODUCT->GETIDPRODUCT()
+            ]);
+        } else {
+
+            $sql->execquery("UPDATE tb_cartsproducts SET dtremoved=NOW() WHERE idcart = :idcart AND idproduct =:idproduct AND 
+            dtremoved IS NULL LIMIT 1 ", [
+                ':idcart' => $this->getidcart(),
+                ':idproduct' => $product->getidproduct()
+            ]);
+        }
     }
     
-    public function save() {
+    public function getProduct() {
         
         $sql=new Sql();
         
-        $result=$sql->select("CALL sp_carts_save(:idcart, :dessessionid, :iduse, :deszipcode, :vlfreight, :nrdays)", [
-            
-        ':idcart'=>$this->getidcart(),
-        ':dessessionid'=>$this->getdessessionid(),
-        ':iduse'=>$this->getiduse(),
-        ':deszipcode'=>$this->getdeszipcode(),
-        ':vlfreight'=>$this->getvlfreight(),
-        ':nrdays'=>$this->getnrdays(),
-            
+        $rows = $sql->select("
+            SELECT b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheigt, b.vllenght, b.weight, COUNT(*) AS nrqtd, SUM(b.
+                vlprice) AS vltotal 
+            FROM tb_cartsproduct a 
+            INNER JOIN tb_products b ON a.idproduct
+            WHERE a.idcart = :idcart AND a.dtremoved IS NULL 
+            GROUP BY b.idproduct, b.desproduct, b.vlprice, b.vlwidth, b.vlheigt, b.vllenght, b.weight
+            ORDER BY b.desproduct
+        ", [
+            ':idcart'=>$this->getidcart()
         ]);
         
-        $this->setData($result[0]);
-        
+        return Product::checklist($rows);
     }
-
-
-    
-
     
 }
 
