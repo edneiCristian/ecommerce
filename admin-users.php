d6f6b4562e6ae00bbb54c9b44311b9eab3c7ea6b
Édnei Cristian <ednei.cristian@gmail.com>
2021-10-15 00:13:34 -0300

    Admin - Categorias - Busca e Paginacao

--- admin-users.php
+++ admin-users.php
@@ -12,7 +12,7 @@ $app->get ( "/admin/users", function () {
 	
 	if($search != ''){
 		
-		$pagination = User::getPageSearch($search, $page, 1);
+		$pagination = User::getPageSearch($search, $page);
 		
 	}else {
 		
