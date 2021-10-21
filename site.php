a4c37a45b1e17112c1f7d52d12329998a042ba24
Édnei Cristian <ednei.cristian@gmail.com>
2021-10-21 14:31:11 -0300

    Ajuste Endereço

--- site.php
+++ site.php
@@ -163,20 +163,14 @@ $app->get ( "/checkout", function () {
 		$cart->getCalculateTotal ();
 	}
 	
-	if (! $address->getdesaddress ())
-		$address->setdesaddress ( '' );
-	if (! $address->getdescomplement ())
-		$address->setdescomplement ( '' );
-	if (! $address->getdesdistrict ())
-		$address->setdesdistrict ( '' );
-	if (! $address->getdescity ())
-		$address->setdescity ( '' );
-	if (! $address->getdesstate ())
-		$address->setdesstate ( '' );
-	if (! $address->getdescountry ())
-		$address->setdescountry ( '' );
-	if (! $address->getdeszipcode ())
-		$address->setdeszipcode ( '' );
+	if (! $address->getdesaddress ()) $address->setdesaddress ( '' );
+	if (! $address->getdesnumber ()) $address->setdesnumber ( '' );
+	if (! $address->getdescomplement ()) $address->setdescomplement ( '' );
+	if (! $address->getdesdistrict ()) $address->setdesdistrict ( '' );
+	if (! $address->getdescity ()) $address->setdescity ( '' );
+	if (! $address->getdesstate ()) $address->setdesstate ( '' );
+	if (! $address->getdescountry ()) $address->setdescountry ( '' );
+	if (! $address->getdeszipcode ()) $address->setdeszipcode ( '' );
 	
 	$page = new Page ();
 	
