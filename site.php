ae613892ca5c19c7238d0273f6e482520d9d4d28
Ednei Cristian <ednei.cristian@gmail.com>
2021-10-25 16:45:54 -0300

    Integração PayPall HTML

--- site.php
+++ site.php
@@ -256,8 +256,20 @@ $app->post ( "/checkout", function () {
 	
 	$order->save ();
 	
-	header ("Location: /order/".$order->getidorder()."/pagseguro");
+	switch ((int)$_POST['payment-method']){
+	    
+	    case 1:
+	        header ("Location: /order/".$order->getidorder()."/pagseguro");
+	        break;
+	        
+	    case 2:
+	        header ("Location: /order/".$order->getidorder()."/paypal");
+	        break;	    
+	}
+		
 	exit;
+	
+	
 } );
 
 
@@ -291,6 +303,30 @@ $app->get("/order/:idorder/pagseguro",function ($idorder){
 	
 	
 });
+
+    $app->get("/order/:idorder/paypal",function ($idorder){
+        
+        User::verifyLogin(false);
+        
+        $order =new Order();
+        
+        $order->get((int)$idorder);
+        
+        $cart = $order->getCart();
+        
+        $page = new Page([
+            
+            'header'=>false,
+            'footer'=>false
+            
+        ]);
+        
+        $page->setTpl("payment-paypal",[
+            'order'=>$order->getValues(),
+            'cart'=>$cart->getValues(),
+            'products'=>$cart->getProducts(),
+          ]);
+    });
 
 
 $app->get ( "/login", function () {
